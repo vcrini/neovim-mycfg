d88e2db76ae71a99d3b09370db76ef2505d62f4b
valerio <vcrini@gmail.com>
2022-12-17 21:05:46 +0100

    first commit

---
+++ init.lua
@@ -0,0 +1,3 @@
+require("vcrini.core.options")
+require("vcrini.core.keymaps")
+require("vcrini.core.colorscheme")
---
+++ lua/vcrini/core/options.lua
@@ -0,0 +1,28 @@
+local opt = vim.opt
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrap
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+-- backspace
+opt.backspace = "indent,eol,start"
