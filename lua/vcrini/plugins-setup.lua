e0068a4a5c671450382e2dc96aa13f534416b43f
valerio <vcrini@gmail.com>
2022-12-18 00:09:19 +0100

    adding two more plugins: comment & nvim-tree (last one has messed graphic)

--- lua/vcrini/plugins-setup.lua
+++ lua/vcrini/plugins-setup.lua
@@ -24,11 +24,21 @@ end
 
 return packer.startup(function(use)
   use("wbthomason/packer.nvim")
-  
+  -- lua functions that many plugins use
+  use("nvim-lua/plenary.nvim") 
   use("bluz71/vim-nightfly-guicolors") -- preferred colorscheme
   -- tmux & split navigator
   use("christoomey/vim-tmux-navigator")
   use("szw/vim-maximizer")
+  -- essential plugins
+  use("tpope/vim-surround")
+  --use("vim-scripts/ReplaceWithRegister")
+  --commenting with gc
+  use("numToStr/Comment.nvim")
+
+  -- file explorer
+
+  use("nvim-tree/nvim-tree.lua")
 
 
   	if packer_bootstrap then
