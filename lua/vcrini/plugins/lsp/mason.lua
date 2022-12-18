e6df8d431ccd51e18d52fe9be0c68c2d21b0231b
valerio <vcrini@gmail.com>
2022-12-18 19:40:13 +0100

    adding linters

--- lua/vcrini/plugins/lsp/mason.lua
+++ lua/vcrini/plugins/lsp/mason.lua
@@ -8,6 +8,13 @@ if not mason_lspconfig_status then
   return
 end
 
+-- import mason-null-ls plugin safely
+local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
+if not mason_null_ls_status then
+  return
+end
+
+
 mason.setup()
 
 mason_lspconfig.setup({
@@ -18,4 +25,15 @@ mason_lspconfig.setup({
     "tailwindcss",
     "sumneko_lua",
   }
+})
+
+mason_null_ls.setup({
+  -- list of formatters & linters for mason to install
+  ensure_installed = {
+    "prettier", -- ts/js formatter
+    "stylua", -- lua formatter
+    "eslint_d", -- ts/js linter
+  },
+  -- auto-install configured formatters & linters (with null-ls)
+  automatic_installation = true,
 })
