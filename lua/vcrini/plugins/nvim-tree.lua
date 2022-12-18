58105fb30f1f7755a29bbd4264e27d9aecc15b83
valerio <vcrini@gmail.com>
2022-12-18 11:57:00 +0100

    commenting a option that I can't understand it's goal

--- lua/vcrini/plugins/nvim-tree.lua
+++ lua/vcrini/plugins/nvim-tree.lua
@@ -18,11 +18,11 @@ nvimtree.setup({
       },
     },
   },
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = false,
-      }
-    }
-  }
+  -- actions = {
+  --   open_file = {
+  --     window_picker = {
+  --       enable = false,
+  --     },
+  --   },
+  -- },
 })
