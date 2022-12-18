2e3d38d8041d9a5a1bc8d89b492697878f1e8be3
valerio <vcrini@gmail.com>
2022-12-18 22:39:45 +0100

    adding git integration

--- lua/vcrini/plugins-setup.lua
+++ lua/vcrini/plugins-setup.lua
@@ -84,6 +84,8 @@ return packer.startup(function(use)
 	-- auto closing
 	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
 	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+	-- git integration
+	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
 	if packer_bootstrap then
 		require("packer").sync()
