c26b9c3fdfde0087461108e19a069907e88f6cb9
valerio <vcrini@gmail.com>
2023-01-09 17:19:41 +0100

    removing unneeded shellcode configuration

--- lua/vcrini/plugins/lsp/mason.lua
+++ lua/vcrini/plugins/lsp/mason.lua
@@ -39,7 +39,6 @@ mason_null_ls.setup({
 		"prettier", -- ts/js formatter
 		"stylua", -- lua formatter
 		"eslint_d", -- ts/js linter
-		"shellcheck", -- bash static analysis
 	},
 	-- auto-install configured formatters & linters (with null-ls)
 	automatic_installation = true,
