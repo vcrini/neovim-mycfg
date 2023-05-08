3076b6801be2db250e5b5d8a2e8cc821e66c2e54
valerio <vcrini@gmail.com>
2023-05-08 17:31:56 +0200

    using right string delimiter

--- lua/vcrini/core/keymaps.lua
+++ lua/vcrini/core/keymaps.lua
@@ -12,7 +12,7 @@ keymap.set("n", "x", '"_x')
 -- vim-maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 -- nvim-tree
-keymap.set("n", "<leader>e", ':NvimTreeToggle<CR>')
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
 -- telescope
 keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
 keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>")
