5eff9a37fe2d2585d15c073a9edc4eb5895fcb88
valerio <vcrini@gmail.com>
2023-09-22 11:53:33 +0200

    without bashls lspconfig was not working

--- lua/vcrini/plugins/lsp/lspconfig.lua
+++ lua/vcrini/plugins/lsp/lspconfig.lua
@@ -137,6 +137,11 @@ return {
       on_attach = on_attach,
     })
 
+    -- configure bash server
+    lspconfig["bashls"].setup({
+      capabilities = capabilities,
+      on_attach = on_attach,
+    })
     -- configure lua server (with special settings)
     lspconfig["lua_ls"].setup({
       capabilities = capabilities,
--- lua/vcrini/plugins/lsp/mason.lua
+++ lua/vcrini/plugins/lsp/mason.lua
@@ -18,26 +18,27 @@ return {
     mason.setup({
       ui = {
         icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-      }
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
     })
 
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "tsserver",
-        "html",
+        "bashls",
         "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "graphql",
         "emmet_ls",
+        "graphql",
+        "html",
+        "lua_ls",
         "prismals",
-        "pyright"
+        "pyright",
+        "svelte",
+        "tailwindcss",
+        "tsserver",
       },
       -- auto-install configured servers (with lspconfig)
       automatic_installation = true, -- not the same as ensure_installed
