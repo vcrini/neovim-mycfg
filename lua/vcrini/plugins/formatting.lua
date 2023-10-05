253746985734f3df8b0297f3cf3e735d0cdf8518
valerio <vcrini@gmail.com>
2023-10-05 17:30:34 +0200

    added bash autoload - wrong extension

--- lua/vcrini/plugins/formatting.lua
+++ lua/vcrini/plugins/formatting.lua
@@ -6,7 +6,7 @@ return {
     conform.setup({
       formatters_by_ft = {
         css = { "prettier" },
-        bash = { "beautysh" },
+        sh = { "beautysh" },
         go = { "gofmt" },
         graphql = { "prettier" },
         html = { "prettier" },
