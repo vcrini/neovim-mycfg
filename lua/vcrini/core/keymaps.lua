11a88bdc68f22762127da1912e9caf39c6b73b59
valerio <vcrini@gmail.com>
2023-10-06 15:28:40 +0200

    adding insert 'tick' for ok

--- lua/vcrini/core/keymaps.lua
+++ lua/vcrini/core/keymaps.lua
@@ -11,6 +11,7 @@ keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("i", "<leader>ok", "✓", { desc = "Insert a tick for ok" })
 
 -- delete single character without copying into register
 -- keymap.set("n", "x", '"_x')
