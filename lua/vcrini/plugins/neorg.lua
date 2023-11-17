5078c950b325d94fa96cca5e0664c3ccf5f9c114
valerio <vcrini@gmail.com>
2023-11-17 19:09:22 +0100

    using neorg

---
+++ lua/vcrini/plugins/neorg.lua
@@ -0,0 +1,37 @@
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+    local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
+    augroup("SetConcealLevel", { clear = true })
+    autocmd("Filetype", {
+      group = "SetConcealLevel",
+      pattern = { "norg" },
+      command = "set conceallevel=2",
+    })
+    require("neorg").setup({
+      load = {
+        ["core.defaults"] = {},
+        ["core.export"] = {},
+        ["core.autocommands"] = {},
+        ["core.concealer"] = {},
+        ["core.summary"] = {},
+        ["core.export.markdown"] = { config = { extension = "md" } },
+        -- ["core.ui"] = {},
+        -- ["core.ui.calendar"] = {},
+        ["core.completion"] = {
+          config = { engine = "nvim-cmp" },
+        },
+        ["core.dirman"] = {
+          config = {
+            workspaces = {
+              notes = "~/Documents/bernini/support/notes",
+            },
+          },
+        },
+      },
+    })
+  end,
+}
