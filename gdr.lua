163924cfe93862bef4a5dbb5ba673db87bc93fa5
valerio <vcrini@gmail.com>
2023-11-19 12:36:15 +0100

    Generate multiple random rolls

--- gdr.lua
+++ gdr.lua
@@ -12,6 +12,13 @@ function M.sum(a)
   end
   return sum
 end
+function M.nd(m, n, side, plus)
+  local r = {}
+  for _ = 1, m do
+    table.insert(r, M.d(n, side, plus)[1])
+  end
+  return r
+end
 function M.d(n, side, plus)
   local r = {}
   for _ = 1, n do
@@ -19,7 +26,8 @@ function M.d(n, side, plus)
     table.insert(r, math.random(1, side))
   end
   table.insert(r, plus)
-  return M.sum(r), r
+  table.insert(r, 1, M.sum(r))
+  return r
 end
 function M.print_cursor(f)
   local pos = api.nvim_win_get_cursor(0)[2]
@@ -27,4 +35,10 @@ function M.print_cursor(f)
   local nline = line:sub(0, pos) .. f .. line:sub(pos + 1)
   api.nvim_set_current_line(nline)
 end
+function M.example()
+  local a, b = M.d(3, 6, 3)
+  return vim.inspect(a, b)
+end
 return M
+--lua print(vim.inspect(require('gdr').d(3,6,3)))
+--lua print(require('gdr').d(10,6,0)[1])
