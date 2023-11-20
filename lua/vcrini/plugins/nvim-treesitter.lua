e1f10385b8638dac8d52db48eeff439eac33c30a
valerio <vcrini@gmail.com>
2023-11-20 17:59:31 +0100

    Removing ts_context_commenstring

--- lua/vcrini/plugins/nvim-treesitter.lua
+++ lua/vcrini/plugins/nvim-treesitter.lua
@@ -52,11 +52,6 @@ return {
           "vim",
           "yaml",
         },
-        -- enable nvim-ts-context-commentstring plugin for commenting tsx and jsx
-        context_commentstring = {
-          enable = true,
-          enable_autocmd = false,
-        },
         -- auto install above language parsers
         auto_install = true,
       })
