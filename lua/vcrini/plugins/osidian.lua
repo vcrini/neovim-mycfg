d9ba864302c20acb7c776fb24364a1b2bdacef6a
valerio <vcrini@gmail.com>
2024-06-13 12:22:35 +0200

    feat: adding better configuration

--- lua/vcrini/plugins/osidian.lua
+++ lua/vcrini/plugins/osidian.lua
@@ -79,6 +79,13 @@ return {
         end,
         opts = { buffer = true },
       },
+      -- Smart action depending on context, either follow link or toggle checkbox.
+      ["<cr>"] = {
+        action = function()
+          return require("obsidian").util.smart_action()
+        end,
+        opts = { buffer = true, expr = true },
+      },
     },
 
     -- Optional, customize how names/IDs for new notes are created.
@@ -181,6 +188,7 @@ return {
         ["x"] = { char = "", hl_group = "ObsidianDone" },
         [">"] = { char = "", hl_group = "ObsidianRightArrow" },
         ["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+        ["!"] = { char = "", hl_group = "ObsidianImportant" },
         -- Replace the above with this if you don't have a patched font:
         -- [" "] = { char = "☐", hl_group = "ObsidianTodo" },
         -- ["x"] = { char = "✔", hl_group = "ObsidianDone" },
@@ -195,16 +203,19 @@ return {
       reference_text = { hl_group = "ObsidianRefText" },
       highlight_text = { hl_group = "ObsidianHighlightText" },
       tags = { hl_group = "ObsidianTag" },
+      block_ids = { hl_group = "ObsidianBlockID" },
       hl_groups = {
         -- The options are passed directly to `vim.api.nvim_set_hl()`. See `:help nvim_set_hl`.
         ObsidianTodo = { bold = true, fg = "#f78c6c" },
         ObsidianDone = { bold = true, fg = "#89ddff" },
         ObsidianRightArrow = { bold = true, fg = "#f78c6c" },
         ObsidianTilde = { bold = true, fg = "#ff5370" },
+        ObsidianImportant = { bold = true, fg = "#d73128" },
         ObsidianBullet = { bold = true, fg = "#89ddff" },
         ObsidianRefText = { underline = true, fg = "#c792ea" },
         ObsidianExtLinkIcon = { fg = "#c792ea" },
         ObsidianTag = { italic = true, fg = "#89ddff" },
+        ObsidianBlockID = { italic = true, fg = "#89ddff" },
         ObsidianHighlightText = { bg = "#75662e" },
       },
     },
